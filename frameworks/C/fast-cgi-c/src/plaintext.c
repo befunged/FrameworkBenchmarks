8a1bfe04579541017e9df72dd143a725d3bb03ea
Andy Drummond <andy.drummond@gmail.com>
2020-12-25 12:22:16 +0000

    get plaintext working without debug

--- frameworks/C/fast-cgi-c/src/plaintext.c
+++ frameworks/C/fast-cgi-c/src/plaintext.c
@@ -4,7 +4,7 @@
 int main (void) {
     while (FCGI_Accept() >= 0) {
         printf("Status: 200 OK\r\n");
-        printf("Content-type: text/html\r\n\r\n");
+        printf("Content-type: text/plain\r\n\r\n");
         printf("Hello, World!");
     }
     return EXIT_SUCCESS;
